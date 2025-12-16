2af610612539e5e2205a2633c19b23d5de436c75
dmitri-lisokonov <dmitri.lisokonov@gmail.com>
2025-12-16 22:26:15 +0100

    added stuff

--- lua/personal/config.lua
+++ lua/personal/config.lua
@@ -1,9 +1,24 @@
 require("personal.keymaps")
 
 lvim.plugins = lvim.plugins or {}
-vim.list_extend(lvim.plugins, require("personal.plugins.extras"))
 vim.list_extend(lvim.plugins, require("personal.plugins.windows"))
 
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = {
+    "*/templates/*.yaml",
+    "*/templates/*.yml",
+    "*/templates/*.tpl",
+    "*/templates/*.txt",
+    "*/charts/*/templates/*.yaml",
+    "*/charts/*/templates/*.yml",
+    "*/charts/*/templates/*.tpl",
+  },
+  callback = function()
+    vim.bo.filetype = "helm"
+  end,
+})
+
 
--- lua/personal/keymaps.lua
+++ lua/personal/keymaps.lua
@@ -1,3 +1,5 @@
 lvim.keys.normal_mode["<leader>bd"] = ":cd -<CR>:pwd<CR>"
-lvim.builtin.which_key.mappings["bd"] = { ":cd -<CR>:pwd<CR>", "Back Directory" }
+
+lvim.builtin.which_key.mappings["b"] = lvim.builtin.which_key.mappings["b"] or { name = "Buffers" }
+lvim.builtin.which_key.mappings["b"]["d"] = { ":cd -<CR>:pwd<CR>", "Back Directory" }
 
