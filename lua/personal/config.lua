430bbbce2ad6d126dd5e9a3c5f20aec0a85c882b
Dmitri Lisokonov <44115633+Dmitri-Lisokonov@users.noreply.github.com>
2025-12-30 12:06:32 +0100

    Clean up telescope and nvimtree settings
    Removed telescope and nvimtree configurations.

--- lua/personal/config.lua
+++ lua/personal/config.lua
@@ -2,22 +2,8 @@ require("personal.keymaps")
 
 lvim.plugins = lvim.plugins or {}
 vim.list_extend(lvim.plugins, require("personal.plugins.windows"))
-lvim.builtin.telescope.defaults.hidden = true
-lvim.builtin.telescope.defaults.no_ignore = true
 vim.opt.relativenumber = true
 vim.opt.number = true
-
-lvim.builtin.telescope.defaults.file_ignore_patterns = {
-  "^.git/",
-}
-
-lvim.builtin.nvimtree.setup.filters = {
-  dotfiles = false,
-}
-
-lvim.builtin.nvimtree.setup.git = {
-  ignore = false,
-}
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = {
