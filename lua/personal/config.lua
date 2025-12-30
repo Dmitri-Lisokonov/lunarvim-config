7fd827fb394d1427c39dfe8658377e080f04530a
Dmitri Lisokonov <44115633+Dmitri-Lisokonov@users.noreply.github.com>
2025-12-30 12:05:35 +0100

    Configure Telescope and NvimTree settings
    Add file ignore patterns for Telescope and configure NvimTree filters.

--- lua/personal/config.lua
+++ lua/personal/config.lua
@@ -7,6 +7,18 @@ lvim.builtin.telescope.defaults.no_ignore = true
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+lvim.builtin.telescope.defaults.file_ignore_patterns = {
+  "^.git/",
+}
+
+lvim.builtin.nvimtree.setup.filters = {
+  dotfiles = false,
+}
+
+lvim.builtin.nvimtree.setup.git = {
+  ignore = false,
+}
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = {
     "*/templates/*.yaml",
