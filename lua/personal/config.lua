0a9cae6f65d6ba1d137633f2a895fc831df276b4
dmitri-lisokonov <dmitri.lisokonov@gmail.com>
2026-04-18 11:35:31 +0200

    fix treesitter health

--- lua/personal/config.lua
+++ lua/personal/config.lua
@@ -1,11 +1,28 @@
 require("personal.keymaps")
 
 lvim.plugins = lvim.plugins or {}
+table.insert(lvim.plugins, {
+  "akinsho/bufferline.nvim",
+  version = "v4.6.1",
+})
+table.insert(lvim.plugins, {
+  "nvimtools/none-ls.nvim",
+  branch = "main",
+})
+table.insert(lvim.plugins, {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+})
 vim.list_extend(lvim.plugins, require("personal.plugins.gruvbox"))
 vim.list_extend(lvim.plugins, require("personal.plugins.windows"))
 lvim.colorscheme = "gruvbox"
 lvim.builtin.illuminate.active = false
 lvim.builtin.treesitter.auto_install = false
+lvim.builtin.treesitter.highlight = {
+  enable = true,
+  disable = { "checkhealth", "help", "markdown", "markdown_inline", "vimdoc" },
+  additional_vim_regex_highlighting = { "checkhealth", "help", "markdown", "markdown_inline", "vimdoc" },
+}
 vim.opt.relativenumber = true
 vim.opt.number = true
 
--- lua/personal/plugins/gruvbox.lua
+++ lua/personal/plugins/gruvbox.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "https://gitlab.com/motaz-shokry/gruvbox.nvim.git",
+    url = "https://gitlab.com/motaz-shokry/gruvbox.nvim.git",
+    name = "gruvbox.nvim",
     priority = 1000,
     config = function()
       require("gruvbox").setup({
